a5813041938ba272002cf66eaa5e518a458609bc
nildiert <niljordan23@gmail.com>
2019-05-31 21:01:23 +0000

    [FIX] Fixed file 3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			key_copy = strdup(key);
 		else
 			return (0);
-		if (!*val_copy && !*key_copy)
+		if (!*val_copy || !*key_copy)
 			return (0);
 		index = key_index((unsigned char *)key, ht->size);
 		new_node->key = key_copy;
