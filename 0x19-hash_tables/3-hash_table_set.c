bf6ebdd5301a38c86501722d60d71a6f732d0b6c
nildiert <niljordan23@gmail.com>
2019-06-01 01:47:18 +0000

    [FIX] Refactorized file 3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,44 +11,73 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	if (ht)
-	{
 /*		int i = 0;*/
-		unsigned long int index;
-		hash_node_t *new_node = NULL;
-		char *val_copy, *key_copy;
-		hash_node_t *copy_node;
+	unsigned long int index;
+	hash_node_t *copy_node = NULL, *new_node = NULL;
 
-		new_node = malloc(sizeof(hash_node_t));
-		val_copy = strdup(value);
-		if ((key && strcmp(key, "") != 0))
-			key_copy = strdup(key);
-		else
-			return (0);
-		if (!*val_copy || !*key_copy)
-			return (0);
+	if (ht && (key && strcmp(key, "") != 0))
+	{
 		index = key_index((unsigned char *)key, ht->size);
-		new_node->key = key_copy;
-		new_node->value = val_copy;
-		new_node->next = NULL;
-		if (ht->array[index] == NULL)
-			ht->array[index] = new_node;
-		else
+		if (ht->array[index] != NULL)
 		{
 			copy_node = ht->array[index];
 			while (copy_node)
 			{
 				if (!strcmp(key, copy_node->key))
 				{
-					copy_node->value = (char *)value;
+					free(copy_node->value);
+					copy_node->value = strdup(value);
+					if (!copy_node->value)
+						return (0);
 					return (1);
 				}
 				copy_node = copy_node->next;
 			}
+			new_node = create_node((char *)key, (char *)value);
+			if (!new_node)
+				return (0);
 			new_node->next = ht->array[index];
 			ht->array[index] = new_node;
+		}
+		else
+		{
+			ht->array[index] = create_node((char *)key, (char *)value);
+			if (!(ht->array[index]))
+				return (0);
 		}
 		return (1);
 	}
 	return (0);
+}
+/**
+ * create_node - hash djb2 algoritm.
+ * @key: key
+ * @value: size of the array of the hash table
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+hash_node_t *create_node(char *key, char *value)
+{
+
+	hash_node_t *new_node = NULL;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (NULL);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+
 }
